0b487521a8051a5d290c2a7fef27bf68d1b5c80c
ET <scorpio@Scorpios-iMac.local>
2021-02-16 23:54:57 +0300

    fx

--- CommandAPI/Controllers/CommandsController.cs
+++ CommandAPI/Controllers/CommandsController.cs
@@ -1,16 +1,26 @@
+using System.Data.Common;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CommandsController:ControllerBase
-    {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] {"this", "is", "the", "test"};
-        }
-    }
+ [Route("api/[controller]")]
+ [ApiController]
+ public class CommandsController : ControllerBase
+ {
+  private readonly ICommandAPIRepo _repository;
+  public CommandsController(ICommandAPIRepo repository)
+  {
+   _repository = repository;
+  }
+
+  [HttpGet]
+  public ActionResult<IEnumerable<string>> Get()
+  {
+   IEnumerable<Models.Command> commandItems = _repository.GetAllCommands();
+   return Ok(commandItems);
+  }
+ }
 }
