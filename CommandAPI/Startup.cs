08201192894ef00240bcb410e14af475df150dca
Scorpio <scorpio @scorpio.et>
2021-02-19 11:19:59 +0300

    fx

--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -8,13 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace CommandAPI
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+          Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
